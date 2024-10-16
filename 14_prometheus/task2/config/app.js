7a22c7899aef91cd25105ee9c61fb4499355a88c
Stas Tarasenko <fieryybird@gmail.com>
2024-10-16 10:47:24 +0200

    add monitoring + aws homeworks

--- 14_prometheus/task2/config/app.js
+++
@@ -1,3 +0,0 @@
-const appPort = process.env.APP_PORT || 3000;
-
-module.exports = appPort;
--- 14_prometheus/task2/config/redis.js
+++
@@ -1,4 +0,0 @@
-const redisAddress = process.env.REDIS_ADDRESS || 'localhost';
-const redisPort = process.env.REDIS_PORT || 6379;
-
-module.exports = { redisAddress, redisPort };
